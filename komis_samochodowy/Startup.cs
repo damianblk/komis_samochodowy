aca8e5a06b4a0b24a524fdd44b3835db9a4be42b
Damian Jarczewski <damian.jarczewski@gmail.com>
2020-07-03 20:39:11 +0200

    clear

--- komis_samochodowy/Startup.cs
+++ komis_samochodowy/Startup.cs
@@ -22,20 +22,7 @@ namespace komis_samochodowy
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
         }
     }
 }
