95d637e1564968ff5f775551507f0708b668f0a3
Damian Jarczewski <damian.jarczewski@gmail.com>
2020-07-03 20:58:33 +0200

    add model

--- komis_samochodowy/Startup.cs
+++ komis_samochodowy/Startup.cs
@@ -22,7 +22,10 @@ namespace komis_samochodowy
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
