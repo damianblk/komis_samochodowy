c51c97f9d147a5cd5687a487d3e37a695dd1f870
Damian Jarczewski <damian.jarczewski@gmail.com>
2020-07-03 21:40:24 +0200

    add models

--- komis_samochodowy/Models/Samochod.cs
+++ komis_samochodowy/Models/Samochod.cs
@@ -1,11 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace komis_samochodowy.Models
+﻿namespace komis_samochodowy.Models
 {
     public class Samochod
     {
+        public int Id { get; set; }
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public string RokProdukcji { get; set; }
+        public string Przebieg { get; set; }
+        public string Pojemnosc { get; set; }
+        public string RodzajPaliwa { get; set; }
+        public string Moc { get; set; }
+        public string Opis { get; set; }
+        public decimal Cena { get; set; }
+        public string ZdjecieUrl { get; set; }
+        public string MiniatrukaUrl { get; set; }
+        public bool SamochodTygodnia { get; set; }
     }
 }
